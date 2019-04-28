57784a227653978692bf612af08f16d3171a7155
Polina.Troshin <polina.troshin@sizmek.com>
2019-04-28 12:18:39 +0300

    more configs

---
+++ tmp/conf.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = {
+    framework: 'jasmine',
+    capabilities: {
+        browserName: 'chrome'
+    },
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['spec/spec.js'],
+    noGlobals: true
+};
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29uZi5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uL2NvbmYudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFFVyxRQUFBLE1BQU0sR0FBWTtJQUMzQixTQUFTLEVBQUUsU0FBUztJQUNwQixZQUFZLEVBQUU7UUFDWixXQUFXLEVBQUUsUUFBUTtLQUN0QjtJQUNELGVBQWUsRUFBRSw4QkFBOEI7SUFDL0MsS0FBSyxFQUFFLENBQUMsY0FBYyxDQUFDO0lBQ3ZCLFNBQVMsRUFBRSxJQUFJO0NBQ2hCLENBQUEifQ==
